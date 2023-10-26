05904ced3acc4ce475d8741d58ea2b7f0c6282d2
devandresilva <iamdevandre@gmail.com>
2023-10-26 08:20:31 -0300

    test: script commented

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
-const pokemonName = document.querySelector('.pokemon-name')
-const pokemonId = document.querySelector('.pokemon-number')
-const pokemonImage = document.querySelector('.pokemon-image')
-const btnNext = document.querySelector('.btn-next')
-const btnPrev = document.querySelector('.btn-prev')
+// const pokemonName = document.querySelector('.pokemon-name')
+// const pokemonId = document.querySelector('.pokemon-number')
+// const pokemonImage = document.querySelector('.pokemon-image')
+// const btnNext = document.querySelector('.btn-next')
+// const btnPrev = document.querySelector('.btn-prev')
 
-//faz a busca no back
-const fetchPokemon = async () => {
-  const response = await fetch(`http://backend:3000/list`);
-  if(response.status === 200){
-    const data = await response.json()
-    return data;
-  }
-};
+// //faz a busca no back
+// const fetchPokemon = async () => {
+//   const response = await fetch(`http://backend:3000/list`);
+//   if(response.status === 200){
+//     const data = await response.json()
+//     return data;
+//   }
+// };
 
-// Transforma o dado vindo do back-end em uma lista com os campos desejados (id, name, imageUrl)
-const listPokemon = async () => {
-  const data = await fetchPokemon(); // Chama a função fetchPokemon para obter os dados
-  const pokemonList = data.map(pokemon => ({
-    id: pokemon.idPokemon,
-    name: pokemon.name,
-    imageUrl: pokemon.imageUrl
-  }));
-  return pokemonList;
-};
+// // Transforma o dado vindo do back-end em uma lista com os campos desejados (id, name, imageUrl)
+// const listPokemon = async () => {
+//   const data = await fetchPokemon(); // Chama a função fetchPokemon para obter os dados
+//   const pokemonList = data.map(pokemon => ({
+//     id: pokemon.idPokemon,
+//     name: pokemon.name,
+//     imageUrl: pokemon.imageUrl
+//   }));
+//   return pokemonList;
+// };
 
-const renderPokemon = async (pokemon) => {
+// const renderPokemon = async (pokemon) => {
 
-  pokemonName.innerHTML = 'Loading...';
-  pokemonId.innerHTML= '';
-  const pokemonList = await listPokemon()
-  if(pokemonList){
-    pokemonImage.style.display = 'block'
-    pokemonName.innerHTML = pokemonList.name[pokemon];
-    pokemonId.innerHTML= pokemonList.id[pokemon];
-    pokemonImage.src = pokemonList.imageUrl[pokemon];
-  } else {
-    pokemonImage.style.display = 'none';
-    pokemonName.innerHTML = 'Not Found';
-    pokemonId.innerHTML= '404';
-  }
-}
+//   pokemonName.innerHTML = 'Loading...';
+//   pokemonId.innerHTML= '';
+//   const pokemonList = await listPokemon()
+//   if(pokemonList){
+//     pokemonImage.style.display = 'block'
+//     pokemonName.innerHTML = pokemonList.name[pokemon];
+//     pokemonId.innerHTML= pokemonList.id[pokemon];
+//     pokemonImage.src = pokemonList.imageUrl[pokemon];
+//   } else {
+//     pokemonImage.style.display = 'none';
+//     pokemonName.innerHTML = 'Not Found';
+//     pokemonId.innerHTML= '404';
+//   }
+// }
 
-renderPokemon(1)
+// renderPokemon(1)
